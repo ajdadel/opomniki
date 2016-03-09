6eb8c8c1cbc8f33c7852e030b3986c3b63f60f2e
Ajda Delic <ajda.delic@gmail.com>
2016-03-09 15:33:04 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,11 +1,23 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	
+	var izvediPrijavo=function(event){
+		var uporabnikIme=document.querySelector("#uporabnisko_ime").value;
+		console.log("Pozdravljen, uporabnik!");
+		
+		document.querySelector("#uporabnik").innerHTML=uporabnikIme;
+		
+		document.querySelector(".pokrivalo").style.display="none";
+		
+		
+	}
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
